6a4f0577b33c840a24c2ddced83c4f1c5c306500
fjinfanteprofile <fjinfante@profile.es>
2024-05-06 15:08:51 +0200

    fixed module issues for standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './modules/shared/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [NavbarComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/modules/shared/navbar/navbar.component.ts
+++ src/app/modules/shared/navbar/navbar.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  
+
 }
