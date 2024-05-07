debca2b460f03ec84d52ade9c3b15f03b43c58b5
fjinfanteprofile <fjinfante@profile.es>
2024-05-07 11:19:52 +0200

    working bookings redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,5 @@ import { BookingsComponent } from './modules/+bookings/bookings.component';
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {path: 'bookings', component: BookingsComponent}
+  // {path: 'profile', component: BookingsComponent}
 ];
