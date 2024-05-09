c81d31fc69127551362625d5b6d61717b391ab0a
fjinfanteprofile <fjinfante@profile.es>
2024-05-09 13:41:52 +0200

    working register service and login

--- src/app/modules/+logregpage/logregpage.component.ts
+++ src/app/modules/+logregpage/logregpage.component.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from './service/login.service'; // Import LoginService
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RegisterService } from './service/register.service';
 
 @Component({
   selector: 'app-logregpage',
   standalone: true,
-  imports: [CommonModule, FormsModule,HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './logregpage.component.html',
   styleUrls: ['./logregpage.component.css']
 })
@@ -19,8 +20,19 @@ export class LogregpageComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private router: Router, private userService: UserService, private loginService: LoginService, private http: HttpClient) {
-    // Provide HttpClientModule here
+  username: string = '';
+  name: string = '';
+  lastName: string = '';
+  dni: string = '';
+  phoneNumber: string = '';
+  address: string = '';
+  age: string = '';
+  role: { id: number } = { id: 1 };
+  active: number = 1;
+
+  constructor(private router: Router, private userService: UserService, private loginService: LoginService, private http: HttpClient
+    , private registerService: RegisterService
+  ) {
     this.http = http;
   }
 
@@ -36,7 +48,7 @@ export class LogregpageComponent {
       password: this.password
     };
 
-    this.loginService.login(this.email, this.password) 
+    this.loginService.login(this.email, this.password)
       .subscribe(
         user => {
           console.log('Logged in user:', user);
@@ -50,6 +62,37 @@ export class LogregpageComponent {
           } else {
             this.loginError = 'An error occurred while logging in. Please try again later.';
           }
+        }
+      );
+  }
+
+  onRegisterSubmit(event: Event): void {
+    event.preventDefault();
+
+    // Implement registration logic here
+    const userData = {
+      username: this.username,
+      name: this.name,
+      lastName: this.lastName,
+      dni: this.dni,
+      phoneNumber: this.phoneNumber,
+      address: this.address,
+      email: this.email,
+      password: this.password,
+      age: this.age,
+      role: this.role,
+      active: this.active
+    };
+
+    this.registerService.registerUser(this.username, this.name, this.password, this.email, this.dni, this.phoneNumber, this.address, this.lastName, this.age, this.role, this.active)
+      .subscribe(
+        response => {
+          console.log('Registration successful:', response);
+          this.showLoginForm = true;
+        },
+        error => {
+          console.error('Registration error:', error);
+          this.loginError = 'An error occurred while registering. Please try again.';
         }
       );
   }
