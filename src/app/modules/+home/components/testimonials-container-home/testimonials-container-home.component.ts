5ace8b114de2969e8444ab6dbf2dd11bcaa5a33b
fjinfanteprofile <fjinfante@profile.es>
2024-05-09 09:10:58 +0200

    solved for in testimonials

--- src/app/modules/+home/components/testimonials-container-home/testimonials-container-home.component.ts
+++ src/app/modules/+home/components/testimonials-container-home/testimonials-container-home.component.ts
@@ -1,17 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-testimonials-container-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './testimonials-container-home.component.html',
   styleUrl: './testimonials-container-home.component.css'
 })
 export class TestimonialsContainerHomeComponent {
 
-  man1: string = 'assets/homepage/profilepic-man.png'
-  man2: string = 'assets/homepage/profilepic-man-2.png'
-  woman1: string = 'assets/homepage/profilepic-woman.png'
-  woman2: string = 'assets/homepage/profilepic-woman-2.png'
+  testimonials = [
+    { name: 'Federica Pepa', img: 'assets/homepage/profilepic-woman-2.png', text: 'I had a wonderful experience learning to drive with FranOnWheels. The instructors were patient and supportive, and they helped me build confidence behind the wheel. I highly recommend them to anyone looking to learn how to drive.' },
+    { name: 'George Clouney', img: 'assets/homepage/profilepic-man.png', text: 'I had a wonderful experience learning to drive with FranOnWheels. The instructors were patient and supportive, and they helped me build confidence behind the wheel. I highly recommend them to anyone looking to learn how to drive.' },
+    { name: 'Stefany Claer', img: 'assets/homepage/profilepic-woman.png', text: 'I had a wonderful experience learning to drive with FranOnWheels. The instructors were patient and supportive, and they helped me build confidence behind the wheel. I highly recommend them to anyone looking to learn how to drive.' },
+    { name: 'Manolo Pepe', img: 'assets/homepage/profilepic-man-2.png', text: 'I had a wonderful experience learning to drive with FranOnWheels. The instructors were patient and supportive, and they helped me build confidence behind the wheel. I highly recommend them to anyone looking to learn how to drive.' }
+  ];
 
 }
