ef5a332fafcc531d8c26eb09553ab4f7afffe9f0
fjinfanteprofile <fjinfante@profile.es>
2024-05-10 09:56:20 +0200

    Renamed dashboard to AdminPanelPage, nomenclature correction

---
+++ src/app/modules/+adminpanelpage/adminpanelpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminpanelpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './adminpanelpage.component.html',
+  styleUrl: './adminpanelpage.component.css'
+})
+export class AdminpanelpageComponent {
+
+}
