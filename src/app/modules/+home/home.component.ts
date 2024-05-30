c1b94f92d4eaca0feddf23b6263ac69d27bc9ca6
fjinfanteprofile <fjinfante@profile.es>
2024-05-30 17:36:02 +0200

    added back to top button

--- src/app/modules/+home/home.component.ts
+++ src/app/modules/+home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ImgContainerHomeComponent } from './components/img-container-home/img-container-home.component';
 import { LocationContainerHomeComponent } from './components/location-container-home/location-container-home.component';
 import { TestimonialsContainerHomeComponent } from './components/testimonials-container-home/testimonials-container-home.component';
@@ -12,7 +12,29 @@ import { FooterComponent } from '../../shared/components/footer/footer.component
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    if (typeof window !== 'undefined') {
+      const mybutton = window.document.getElementById("btn-back-to-top") as HTMLElement;
 
+      window.onscroll = function () {
+        scrollFunction();
+      };
 
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          mybutton.classList.add("visible");
+        } else {
+          mybutton.classList.remove("visible");
+        }
+      }
+
+      mybutton.addEventListener("click", backToTop);
+
+      function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+    }
+  }
 }
