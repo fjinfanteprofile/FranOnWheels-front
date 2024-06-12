52b0380a504c887b7ef9cd68428d0b5fd7a126bf
Fran <fjinfante@profile.es>
2024-06-12 19:24:17 +0200

    validation profile update

--- src/app/modules/+profilepage/profilepage.component.ts
+++ src/app/modules/+profilepage/profilepage.component.ts
@@ -3,21 +3,36 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { UserService } from '../../shared/service/user.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TOAST_MSGS, TOAST_TYPES } from '../../shared/components/constants';
 import { ToastService } from '../../shared/service/toast.service';
 
 @Component({
   selector: 'app-profilepage',
   standalone: true,
-  imports: [CommonModule,RouterModule, FormsModule],
+  imports: [CommonModule,RouterModule, FormsModule,ReactiveFormsModule],
   templateUrl: './profilepage.component.html',
   styleUrls: ['./profilepage.component.css']
 })
 export class ProfilepageComponent implements OnInit {
   user: any;
+  profileForm: FormGroup;
 
-  constructor(private userService: UserService, private router: Router, private toastService : ToastService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private toastService: ToastService,
+    private formBuilder: FormBuilder
+  ) {
+    this.profileForm = this.formBuilder.group({
+        name: ['', [Validators.required, Validators.maxLength(20)]],
+        lastName: ['', [Validators.required, Validators.maxLength(30)]],
+        dni: ['', [Validators.required, Validators.pattern(/^\d{8}[A-Z]$/), Validators.maxLength(9)]],
+        phoneNumber: ['', [Validators.required, Validators.maxLength(15)]], 
+        address: ['', [Validators.required, Validators.maxLength(60)]], 
+        email: ['', [Validators.required, Validators.email, Validators.maxLength(40)]] 
+      });
+  }
 
   ngOnInit(): void {
     this.user = this.userService.getUser();
